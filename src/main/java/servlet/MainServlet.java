4bcbcea334065a4680ae600bd4bf3cf3fa75ea50
Simon Polushkin <105408053+SimonIsNotAvailable@users.noreply.github.com>
2023-01-09 11:07:57 +0500

    MainServlet refactoring added

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
   private PostController controller;
+  private final static String METHOD_GET = "GET";
+  private final static String METHOD_POST = "POST";
+  private final static String METHOD_DELETE = "DELETE";
 
   @Override
   public void init() {
@@ -23,24 +26,24 @@ public class MainServlet extends HttpServlet {
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
+      
+      if (method.equals(METHOD_GET) && path.equals("/api/posts")) {
         controller.all(resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+      if (method.equals(METHOD_GET) && path.matches("/api/posts/\\d+")) {
+        
+        String id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.getById(id, resp);
         return;
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
+      if (method.equals(METHOD_POST) && path.equals("/api/posts")) {
         controller.save(req.getReader(), resp);
         return;
       }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+      if (method.equals(METHOD_DELETE) && path.matches("/api/posts/\\d+")) {
+        
+        String id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.removeById(id, resp);
         return;
       }
